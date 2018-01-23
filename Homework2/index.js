c30306d65f2a7ce11050f398d1cec22f155706be
Bratok3000 <Bratok3000@gmail.com>
2018-01-23 23:22:46 +0200

    Lesson 5

---
+++ Homework2/index.js
@@ -0,0 +1,14 @@
+var meneger = {
+    firstName: "Bill",
+    lastName: "Salivan",
+    age: 27
+} 
+
+console.log(meneger.firstname + " " + meneger.lastName + " " + meneger.age);
+
+var secretar = new Object();
+secretar.name = "Bob";
+secretar.sername = "Wasovski";
+secretar.age = 19;
+
+console.log(secretar.name + " " + secretar.firstname + " " + secretar.age);
