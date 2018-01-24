ec2827e5fc2e98030ab022faae9e8f7d585e671d
Bratok3000 <bratok3000@gmail.com>
2018-01-24 14:05:13 +0200

    updated homework2

--- Homework2/index.js
+++ Homework2/index.js
@@ -1,14 +1,34 @@
-var meneger = {
-    firstName: "Bill",
-    lastName: "Salivan",
-    age: 27
-} 
+var document1 = {
+    header: "headerDocument",
+    body: "bodyDocument",
+    footer: "footerDocument",
+    date: "dateDocument",
 
-console.log(meneger.firstname + " " + meneger.lastName + " " + meneger.age);
+    app:{
+        header: "headerapp",
+        body: "bodyapp",
+        footer: "footerapp",
+        date: "today" 
+    },
 
-var secretar = new Object();
-secretar.name = "Bob";
-secretar.sername = "Wasovski";
-secretar.age = 19;
+    editor: function(){
+        
+        var a = prompt("Vvedite header: ");
+        var b = prompt("Vvedite body: ");
+        var c = prompt("Vvedite footer: ");
+        var d = prompt("Vvedite date: ");
 
-console.log(secretar.name + " " + secretar.firstname + " " + secretar.age);
+        document1.app.header = a;
+        document1.app.body = b;
+        document1.app.footer = c;
+        document1.app.date = d;
+    },
+
+    show: function(){
+        console.log(document1.app.header + " " +  document1.app.body + " " + document1.app.footer + " " + document1.app.date);
+    }
+}
+
+document1.editor();
+document1.show();
+
